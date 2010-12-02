04d4aa1fd9991c2c7ad9942efd36c9fcb1581ebb
Angamuthu <angamuthu.g@multunus.com>
2010-12-02 16:10:58 +0530

    done with operator validation

--- css/blueprint/calc.js
+++ css/blueprint/calc.js
@@ -6,27 +6,48 @@ function getElement(input) {
     return document.getElementById(input);
 }
 
-//Operand.inheriteFrom(Input);
-//Operand.prototype = new Input;
-//Operand.prototype.constructor = new Input();
 function Operand(inputField) {            
     this.inheritFrom = Input;
     this.inheritFrom(getElement(inputField));
-    this.helpField = getElement(inputField + "-helptext");    
+    this.helpText = getElement(inputField + "-helptext");    
 }
 
+function Operator(inputField) {            
+    this.inheritFrom = Input;
+    this.inheritFrom(getElement(inputField));
+    this.helpText = getElement(inputField + "-helptext");    
+}
+
+var operatorArray = ['+', '-', '/', '*']; 
 Operand.prototype.validate = function(input) {    
     if (isNaN(input.value) || input.value == "") {	
-        this.helpField.innerHTML = "Enter numbers only";
+        this.helpText.innerHTML = "Enter numbers only";
 	return false;
     } else {	
-	this.helpField.innerHTML = "";	
+	this.helpText.innerHTML = "";	
 	return true;
     }
 }
 
+Operator.prototype.validate = function(input) {    
+    if (operatorArray.indexOf(input.value) == -1) {
+	this.helpText.innerHTML = "Enter operator only";
+	return false;
+    } else {	
+	this.helpText.innerHTML = "";	
+	return true;
+    }
+}
 function validateOperand(inputField) {    
-    objOperand = new Operand(inputField);
-    objOperand.validate(objOperand.inputField);
+    objOperand = new Operand(inputField);      
+    objOperand.validate(objOperand.inputField);    
 }    
+
+function validateOperator(inputField) {        
+    objOperator = new Operator(inputField);
+    objOperator.validate(objOperator.inputField);
+}
+
+function evaluate() { 
+}
 
